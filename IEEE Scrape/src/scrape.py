890f994ca6cac0f48fa87e278173f6b2e14583f2
Aditya Agarwal <adityag2511@gmail.com>
2016-05-03 02:37:57 +0530

    Getting the years

--- IEEE Scrape/src/scrape.py
+++ IEEE Scrape/src/scrape.py
@@ -2,9 +2,12 @@ from bs4 import BeautifulSoup
 import sys,urllib2
 
 
-baseurl = 'http://ieeexplore.ieee.org/xpl/aboutJournal.jsp?punumber=6287639'
+baseurl = 'http://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber=85'
 hdr = {'User-Agent':'Mozilla/5.0'}
 req = urllib2.Request(baseurl,headers=hdr)
 page = urllib2.urlopen(req)	
 soup = BeautifulSoup(page, 'html.parser')
-print(soup)
+volumes = soup.find("div",{"class":'volumes'})
+years = volumes.find_all('ul')
+for year in years:
+	print(year['id'])
