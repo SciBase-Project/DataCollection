616c2584970f5f1693b4ea64c86cf4058a3b2fbd
Aditya Agarwal <adityag2511@gmail.com>
2016-05-04 00:40:34 +0530

    article number

--- IEEE Scrape/src/scrape.py
+++ IEEE Scrape/src/scrape.py
@@ -11,7 +11,8 @@ import json
 
 # printing data of the journal
 
-# baseurl = 'http://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber=85'
+eurl = 'http://ieeexplore.ieee.org'
+# url = ''
 # hdr = {'User-Agent':'Mozilla/5.0'}
 # req = urllib2.Request(baseurl,headers=hdr)
 # page = urllib2.urlopen(req)	
@@ -23,3 +24,23 @@ import json
 # 	print(year['id'])
 # 	for issue in issues:
 # 		print(issue['href'])
+
+baseurl = 'http://ieeexplore.ieee.org/xpl/tocresult.jsp?isnumber=6336544&punumber=6287639'
+hdr = {'User-Agent':'Mozilla/5.0'}
+req = urllib2.Request(baseurl,headers=hdr)
+page = urllib2.urlopen(req)	
+soup = BeautifulSoup(page, 'html.parser')
+
+link1 = soup.find('input',{'id':'oqs'})
+link0 = soup.find('input',{'id':'submitUrl'})
+total_number = soup.find('div',{'class':'results-display'}).find_all('b')[1].get_text()
+
+newurl = eurl + link0['value'] + link1['value'] + '&rowsPerPage=' + total_number
+
+req = urllib2.Request(newurl,headers=hdr)
+page = urllib2.urlopen(req).read()
+fsoup = BeautifulSoup(page, 'html.parser')
+articles = fsoup.find('ul',{'class':'results'}).find_all('li')
+for article in articles:
+	if article.find('h3').find('a') != None:
+		print(article.find('span').find('input')['id'])
